99c37a6fef454640c6b1036de1ab630beef24ac4
Chiel Timmermans <chiel1997@hotmail.com>
2018-06-18 22:12:07 +0200

    ADO.net crud operations

---
+++ DB2/DatabaseHandler.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Data.SqlClient;
+
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class DatabaseHandler
+    {
+        private string connectionString;
+        public DatabaseHandler(string server, string databaseName, string username, string password)
+        {
+            Console.WriteLine("Starting DatabaseHandler");
+            this.connectionString = @"Data Source=" + server + ";Initial Catalog=" + databaseName + ";User id=" + username + ";Password=" + password + ";";
+        }
+
+        public void countRows()
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                string tableCommand = "SELECT count(*) FROM users";
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(tableCommand, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}", reader[0]);
+                    }
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+        }
+
+        public string deleteQueries(int count, int id)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                string tableCommand;
+                int max = 999;
+                int index = 0;
+                try
+                {
+                    connection.Open();
+
+                    for (int idx = 0; idx <= (count / 1000); idx++)
+                    {
+                        tableCommand = "DELETE FROM users WHERE email in (";
+
+                        if (idx == (count / 1000))
+                        {
+                            max = (count % 1000) - 1;
+                        }
+                        if (max != -1)
+                        {
+                            for (int idx2 = 0; idx2 < max; idx2++)
+                            {
+                                tableCommand += "'" + id + "email" + index + "@stenden.com',";
+                                index++;
+                            }
+
+                            tableCommand += "'" + id + "email" + index + "@stenden.com');";
+                            index++;
+                            SqlCommand command = new SqlCommand(tableCommand, connection);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            return elapsedTime;
+        }
+
+        public string updateQueries(int count, int id)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                String tableCommand = "UPDATE users set password = 'pilsbaas' where email = @email";
+
+
+                try
+                {
+                    connection.Open();
+
+                    for (int idx = 0; idx < count; idx++)
+                    {
+                        SqlCommand command = new SqlCommand(tableCommand, connection);
+
+                        command.Parameters.AddWithValue("@email", id + "email" + idx + "@stenden.com");
+                        command.ExecuteNonQuery();
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            return elapsedTime;
+        }
+
+        public string selectQueries(int count)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                String queryString = "SELECT TOP " + count + " * FROM users;";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    string data = "";
+                    while (reader.Read())
+                    {
+                        data = String.Format("{0} {1} {2}", reader[0], reader[1], reader[2]);
+                    }
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            return elapsedTime;
+        }
+
+        public string createQueries(int count, int id)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                string tableCommand;
+                int max = 999;
+                int index = 0;
+                try
+                {
+                    connection.Open();
+
+                    for (int idx = 0; idx <= (count / 1000); idx++)
+                    {
+                        tableCommand = "insert into users(email, [password]) values";
+
+                        if (idx == (count / 1000))
+                        {
+                            max = (count % 1000) - 1;
+                        }
+
+                        if (max != -1)
+                        {
+                            for (int idx2 = 0; idx2 < max; idx2++)
+                            {
+                                tableCommand += "('" + id + "email" + index + "@stenden.com','admin1234'),";
+                                index++;
+                            }
+
+                            tableCommand += "('" + id + "email" + index + "@stenden.com','admin1234');";
+                            index++;
+
+                            SqlCommand command = new SqlCommand(tableCommand, connection);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            return elapsedTime;
+        }
+
+        public void createDatabase()
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+
+                String tableCommand = "CREATE TABLE " +
+                                    "users (" +
+                                        "id INT NOT NULL UNIQUE IDENTITY(1,1)," +
+                                        "email VARCHAR(255) NOT NULL UNIQUE," +
+                                        "[password] VARCHAR(72) NOT NULL," +
+                                        "failedLoginAttempts TINYINT NOT NULL DEFAULT 0," +
+                                        "emailIsActivated TINYINT NOT NULL DEFAULT 0," +
+                                        "created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+                                        "PRIMARY KEY (id)" +
+                                    ");";
+
+                try
+                {
+
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(tableCommand, connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+
+            }
+        }
+
+        public void getVersion()
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine("ServerVersion: {0}", connection.ServerVersion);
+                Console.WriteLine("Database: {0}", connection.Database);
+
+            }
+        }
+    }
+}
---
+++ DB2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADO.net programma om 1, 1000, 100000 en 1000000 crud operations te doen op de tabel users\n");
+            TextReader tIn = Console.In;
+            DatabaseHandler dbhandler = new DatabaseHandler("DESKTOP-OR069KP\\SQLEXPRESS", "DB", "root", "");
+
+            dbhandler.getVersion();
+            //dbhandler.createDatabase();
+
+            Console.WriteLine("Create Queries: creating 1 row took: " + dbhandler.createQueries(1, 1) + "ms");
+            Console.WriteLine("Create Queries: creating 1000 row took: " + dbhandler.createQueries(1000, 2) + "ms");
+            Console.WriteLine("Create Queries: creating 100000 row took: " + dbhandler.createQueries(100000, 3) + "ms");
+            Console.WriteLine("Create Queries: creating 1000000 row took: " + dbhandler.createQueries(1000000, 4) + "ms");
+
+            Console.Write("Na deze input moeten er 1101001 rows zijn. Ingevoerde velden: ");
+            dbhandler.countRows();
+            Console.WriteLine("");
+            Console.WriteLine("Select Queries: selecting 1 row took: " + dbhandler.selectQueries(1) + "ms");
+            Console.WriteLine("Select Queries: selecting 1000 row took: " + dbhandler.selectQueries(1000) + "ms");
+            Console.WriteLine("Select Queries: selecting 100000 row took: " + dbhandler.selectQueries(100000) + "ms");
+            Console.WriteLine("Select Queries: selecting 1000000 row took: " + dbhandler.selectQueries(1000000) + "ms");
+
+            //Console.WriteLine("Update Queries: updating 1 row took: " + dbhandler.updateQueries(1, 1) + "ms");
+            //Console.WriteLine("Update Queries: updating 1000 row took: " + dbhandler.updateQueries(1000, 2) + "ms");
+            //Console.WriteLine("Update Queries: updating 100000 row took: " + dbhandler.updateQueries(100000, 3) + "ms");
+            //Console.WriteLine("Update Queries: updating 1000000 row took: " + dbhandler.updateQueries(1000000, 4) + "ms");
+
+            Console.WriteLine("Delete Queries: deleting 1 row took: " + dbhandler.deleteQueries(1, 1) + "ms");
+            Console.WriteLine("Delete Queries: deleting 1000 row took: " + dbhandler.deleteQueries(1000, 2) + "ms");
+            Console.WriteLine("Delete Queries: deleting 100000 row took: " + dbhandler.deleteQueries(100000, 3) + "ms");
+            Console.WriteLine("Delete Queries: deleting 1000000 row took: " + dbhandler.deleteQueries(1000000, 4) + "ms");
+            Console.Write("Na deze delete queries moeten er nog 0 rows zijn. Ingevoerde velden: ");
+            dbhandler.countRows();
+            Console.WriteLine("");
+
+            Console.WriteLine("Done benchmarking");
+            while (true)
+            {
+                String name = tIn.ReadLine();
+                if (name == ".")
+                {
+                    return;
+                }
+            }
+
+        }
+    }
+}
